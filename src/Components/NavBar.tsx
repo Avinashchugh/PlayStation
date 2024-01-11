660c41fc007ad82ee4ab16751cb4647a6fbd8e14
Avinashchugh <Avinashkumar.bscsf21@iba-suk.edu.pk>
2024-01-12 02:49:21 +0500

    initial commit

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,12 +2,15 @@ import { HStack } from '@chakra-ui/react';
 import Logo from '../assets/download.webp'
 import SwitchMode from './SwitchMode';
 import SearchInput from './SearchInput';
-const NavBar = () => {
+interface props{
+  onSearch: (searchText:string)=>void;
+}
+const NavBar = ({onSearch}:props) => {
   return (
     <div>
         <HStack justifyContent='space-between' padding='10px'>
             <img src={Logo} width={50} />
-            <SearchInput/>
+            <SearchInput onSearch={onSearch}/>
             <SwitchMode/>
         </HStack>
     </div>
--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -1,15 +1,28 @@
-import { Input,InputGroup,InputLeftElement } from '@chakra-ui/react'
-import { BsSearch } from 'react-icons/bs'
-const SearchInput = () => {
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+interface props{
+  onSearch: (searchText:string)=>void;
+}
+const SearchInput = ({onSearch}: props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
     <div>
-    <InputGroup>
-    <InputLeftElement children={<BsSearch/>}/>
-    <Input borderRadius={20} placeholder='Search games' variant='filled' ></Input>
-    </InputGroup>
-      
+      <form onSubmit={(e) =>{
+        e.preventDefault();
+        if(ref.current) onSearch(ref.current.value)
+      }}>
+        <InputGroup>
+          <InputLeftElement children={<BsSearch />} />
+          <Input ref={ref}
+            borderRadius={20}
+            placeholder="Search games"
+            variant="filled"
+          ></Input>
+        </InputGroup>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
